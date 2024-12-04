741e609edcb921382b1b30a0129ec57970c8bbe8
Philip <philipjonsson02@gmail.com>
2024-12-04 10:19:17 +0100

    feat: add initial package.json and implement date utility functions

--- script.js
+++ script.js
@@ -1,131 +1,16 @@
-//Code from prompt 1 and 2
-const inquirer = require('inquirer');
-const fs = require('fs-extra');
-const path = require('path');
+const schedule = require('node-schedule');
+const fs = require('fs');
 
-async function mainMenu() {
-  try {
-    const answers = await inquirer.prompt([
-      {
-        type: 'list',
-        name: 'action',
-        message: 'Välj en åtgärd:',
-        choices: [
-          'Flytta mellan mappar',
-          'Omdöp alla filer i en mapp',
-          'Partiell omdöpning av filer',
-          'Avsluta'
-        ]
-      }
-    ]);
-
-    switch (answers.action) {
-      case 'Flytta mellan mappar':
-        await moveBetweenFolders();
-        break;
-      case 'Omdöp alla filer i en mapp':
-        await renameAllFiles();
-        break;
-      case 'Partiell omdöpning av filer':
-        await partialRenameFiles();
-        break;
-      case 'Avsluta':
-        console.log('Avslutar...');
-        process.exit();
-    }
-  } catch (error) {
-    console.error('Ett fel uppstod:', error);
-  }
-}
-
-async function moveBetweenFolders() {
-  try {
-    const answers = await inquirer.prompt([
-      {
-        type: 'input',
-        name: 'source',
-        message: 'Ange källmappens sökväg:'
-      },
-      {
-        type: 'input',
-        name: 'destination',
-        message: 'Ange destinationsmappens sökväg:'
-      }
-    ]);
-
-    await fs.move(answers.source, answers.destination);
-    console.log('Mapparna har flyttats.');
-  } catch (err) {
-    console.error('Fel vid flyttning av mappar:', err);
-  }
-
-  mainMenu();
-}
-
-async function renameAllFiles() {
-  try {
-    const answers = await inquirer.prompt([
-      {
-        type: 'input',
-        name: 'folder',
-        message: 'Ange mappens sökväg:'
-      },
-      {
-        type: 'input',
-        name: 'newName',
-        message: 'Ange det nya namnet för filerna:'
-      }
-    ]);
-
-    const files = await fs.readdir(answers.folder);
-    files.forEach((file, index) => {
-      const ext = path.extname(file);
-      const newFileName = `${answers.newName}_${index + 1}${ext}`;
-      fs.renameSync(path.join(answers.folder, file), path.join(answers.folder, newFileName));
+// Function to perform the booked operation
+function bookedOperation() {
+    const timestamp = new Date().toISOString();
+    fs.appendFile('log.txt', `Booked operation executed at ${timestamp}\n`, (err) => {
+        if (err) throw err;
+        console.log(`Booked operation executed at ${timestamp}`);
     });
-    console.log('Alla filer har omdöpts.');
-  } catch (err) {
-    console.error('Fel vid omdöpning av filer:', err);
-  }
-
-  mainMenu();
 }
 
-async function partialRenameFiles() {
-  try {
-    const answers = await inquirer.prompt([
-      {
-        type: 'input',
-        name: 'folder',
-        message: 'Ange mappens sökväg:'
-      },
-      {
-        type: 'input',
-        name: 'pattern',
-        message: 'Ange regex-mönstret för att matcha filer:'
-      },
-      {
-        type: 'input',
-        name: 'replacement',
-        message: 'Ange ersättningstexten:'
-      }
-    ]);
+// Schedule the booked operation to run every minute
+const job = schedule.scheduleJob('* * * * *', bookedOperation);
 
-    const regex = new RegExp(answers.pattern);
-    const files = await fs.readdir(answers.folder);
-    files.forEach((file) => {
-      if (regex.test(file)) {
-        const newFileName = file.replace(regex, answers.replacement);
-        fs.renameSync(path.join(answers.folder, file), path.join(answers.folder, newFileName));
-      }
-    });
-    console.log('Filerna har omdöpts delvis.');
-  } catch (err) {
-    console.error('Fel vid partiell omdöpning av filer:', err);
-  }
-
-  mainMenu();
-}
-
-mainMenu();
-//--------------------------------------------
+console.log('Scheduled booked operation to run every minute.');
